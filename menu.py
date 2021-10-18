98d440ab9af2a3e694c194869235527bedb37c45
erwin <merwinfattah@gmail.com>
2021-10-18 09:50:28 +0700

    add schemas.py , crud.py and add endpoint in menu.py

--- menu.py
+++ menu.py
@@ -1,10 +1,63 @@
-from fastapi import FastAPI,HTTPException
+from datetime import timedelta
+from fastapi import FastAPI,HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from . import crud, schemas
 import json
+
 
 with open("menu.json","r") as read_file:
 	data = json.load(read_file)
 
+fake_users_db = {
+
+}
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
 app = FastAPI()
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, crud.SECRET_KEY, algorithms=[crud.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = username
+    except JWTError:
+        raise credentials_exception
+    user = crud.get_user(fake_users_db, username=token_data)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@app.post("/token", response_model=schemas.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = crud.authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = crud.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/")
+async def read_users_me(current_user: schemas.User = Depends(get_current_user)):
+    return current_user
+
 
 @app.get('/menu/{item_id}') # Lihat  menu
 async def read_menu(item_id: int):
